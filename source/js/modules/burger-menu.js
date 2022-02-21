565c8c579912ab1d8b1f5f7c703ac5db706335a5
Yaroslav Kovalev <yakovaldos@yandex.ru>
2022-02-21 23:34:20 +0300

    fixed bugs

--- source/js/modules/burger-menu.js
+++ source/js/modules/burger-menu.js
@@ -1,4 +1,5 @@
 // Burger menu - Start -
+
 const initBurgerMenu = () => {
   const iconMenu = document.querySelector('.menu__icon');
   if (iconMenu) {
@@ -7,6 +8,8 @@ const initBurgerMenu = () => {
     iconMenu.addEventListener('click', () => {
       iconMenu.classList.toggle('is-active');
       menuBody.classList.toggle('is-active');
+      document.body.classList.toggle('is-locked');
+      // Фикс для перекрытия боковым меню слайдера с партнёрами, если он будет
       if (slider) {
         setTimeout(() => {
           slider.classList.toggle('z-index--1');
--- source/js/modules/init-accordion.js
+++ source/js/modules/init-accordion.js
@@ -2,6 +2,7 @@ import {Accordions} from './accordions';
 let accordions;
 
 const initAccordions = () => {
+  
   accordions = new Accordions();
   // Используйте в разработке экспортируемую переменную accordions, window сделан для бэкэнда
   window.accordions = accordions;
